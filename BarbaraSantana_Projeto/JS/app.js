ccd212c7c3d16562bb646164864438609aedc973
Bárbara Santana <55034150+babqs@users.noreply.github.com>
2021-12-23 11:35:54 -0300

    Add files via upload

--- BarbaraSantana_Projeto/JS/app.js
+++ BarbaraSantana_Projeto/JS/app.js
@@ -4,15 +4,15 @@ document.getElementById('contato').addEventListener('click', contato);
 document.getElementById('btnEnviar').addEventListener('click', formValidation);
 
 function home() {
-    window.location.href = "/BarbaraSantana_Projeto/HTML/home.html";
+    window.location.href = "/HTML/home.html";
 }
 
 function pessoal() {
-    window.location.href = "/BarbaraSantana_Projeto/HTML/pessoal.html";
+    window.location.href = "/HTML/pessoal.html";
 }
 
 function contato() {
-    window.location.href = "/BarbaraSantana_Projeto/HTML/contato.html";
+    window.location.href = "/HTML/contato.html";
 }
 
 function formValidation() {
